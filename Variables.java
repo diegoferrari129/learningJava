36e5739066a48b4b1866f6f88b2969cabbbd534f
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-03 22:47:00 +0100

    [UPD] added variables in "Variables.java"

--- Variables.java
+++ Variables.java
@@ -1,10 +1,21 @@
 /*
  * Dichiarare una variabile, assegnarne un valore e inizializzare una variabile
  * Una variabile è definita dal tipo di dato che contiene e dal nome della variabile
+ * LowerCamelCase per le variabili
+ * Maiuscolo con lo snake_case per le costanti
  */
 
 public class Variables {
     public static void main(String[] args) {
+        int x;
+        x = 1;
+        System.out.println(x);
+        // oppure
+        int y = 2;
+        System.out.println(y);
 
+        // cambia il valore della variabile x
+        x = 3;
+        System.out.println(x);
     }
 }
