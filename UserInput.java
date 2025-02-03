e2be5542eae75f93a6705a882faa23b6fb0e47db
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-03 23:46:51 +0100

    [FEAT] scaffolding UserInput.java

---
+++ UserInput.java
@@ -0,0 +1,9 @@
+/*importo lo scanner dall'utility di java per poterlo richiamare 
+ * creo l'oggetto scanner
+*/
+
+public class UserInput {
+    public static void main(String[] args) {
+
+    }
+}
