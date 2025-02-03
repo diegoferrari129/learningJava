d131366a8f7fd5dcec9d4dd1c4cbbc89fbec92fc
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-03 22:25:44 +0100

    [UPD] printed a message on screen

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // dico al sistema di mandare qualcosa fuori e di stamparlo
+        System.out.print("Hello world!");
 
+        // creo una nuova riga con \n
+        System.out.print("Hello world! \n");
+        // oppure
+        System.out.println("Hello world!");
     }
 }
