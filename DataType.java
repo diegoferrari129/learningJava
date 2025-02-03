7d6c239701e0e0a0de8a1bae696908438dcd01e0
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-03 23:25:07 +0100

    [FEAT] scaffolding to DataType.java

---
+++ DataType.java
@@ -0,0 +1,12 @@
+/*
+ * Primitivi
+ * boolean, char, byte, short, int, long, float, double
+ * 
+ * Reference
+ * String
+ */
+public class DataType {
+    public static void main(String[] args) {
+
+    }
+}
