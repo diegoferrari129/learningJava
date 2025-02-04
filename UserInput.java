50ef969102e08d344706f4824e05288be9d8a7fd
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-04 22:43:05 +0100

    [UPD] added name input

--- UserInput.java
+++ UserInput.java
@@ -7,5 +7,10 @@ import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci il tuo nome: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ciao: " + name);
     }
 }
