2d972972b94de259808dd1ee912c6d2b191839aa
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-07 20:57:18 +0100

    [UPD] added content

--- Operators.java
+++ Operators.java
@@ -1,5 +1,9 @@
+// da il resto della divisione
 public class Operators {
     public static void main(String[] args) {
-
+        int a = 10;
+        int b = 3;
+        int c = a % b;
+        System.out.println(c);
     }
 }
