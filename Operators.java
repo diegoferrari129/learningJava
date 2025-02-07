f7e20dfac4c83fc61b229e2ce9a5820c7dd4f985
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-02-07 20:58:43 +0100

    [UPD] added shorthand example

--- Operators.java
+++ Operators.java
@@ -1,9 +1,15 @@
-// da il resto della divisione
+// % da il resto della divisione
 public class Operators {
     public static void main(String[] args) {
+        // % da il resto della divisione
         int a = 10;
         int b = 3;
         int c = a % b;
         System.out.println(c);
+
+        // shorthand operator
+        int d = 10;
+        d += 3;
+        System.out.println(d);
     }
 }
